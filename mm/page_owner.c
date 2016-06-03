db13fd12c9161b172265b2c65c700fae406c529c
Yang Shi <yang.shi@linaro.org>
2016-06-03 14:55:38 -0700

    mm: check the return value of lookup_page_ext for all call sites
    commit f86e4271978bd93db466d6a95dad4b0fdcdb04f6 upstream.
    
    Per the discussion with Joonsoo Kim [1], we need check the return value
    of lookup_page_ext() for all call sites since it might return NULL in
    some cases, although it is unlikely, i.e.  memory hotplug.
    
    Tested with ltp with "page_owner=0".
    
    [1] http://lkml.kernel.org/r/20160519002809.GA10245@js1304-P5Q-DELUXE
    
    [akpm@linux-foundation.org: fix build-breaking typos]
    [arnd@arndb.de: fix build problems from lookup_page_ext]
      Link: http://lkml.kernel.org/r/6285269.2CksypHdYp@wuerfel
    [akpm@linux-foundation.org: coding-style fixes]
    Link: http://lkml.kernel.org/r/1464023768-31025-1-git-send-email-yang.shi@linaro.org
    Signed-off-by: Yang Shi <yang.shi@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -109,6 +109,7 @@ void __reset_page_owner(struct page *page, unsigned int order)
 static inline bool check_recursive_alloc(struct stack_trace *trace,
 					unsigned long ip)
 {
+<<<<<<< HEAD
 	int i, count;
 
 	if (!trace->nr_entries)
@@ -125,6 +126,10 @@ static inline bool check_recursive_alloc(struct stack_trace *trace,
 static noinline depot_stack_handle_t save_stack(gfp_t flags)
 {
 	unsigned long entries[PAGE_OWNER_STACK_DEPTH];
+=======
+	struct page_ext *page_ext = lookup_page_ext(page);
+
+>>>>>>> e34e744... mm: check the return value of lookup_page_ext for all call sites
 	struct stack_trace trace = {
 		.nr_entries = 0,
 		.entries = entries,
@@ -132,6 +137,9 @@ static noinline depot_stack_handle_t save_stack(gfp_t flags)
 		.skip = 0
 	};
 	depot_stack_handle_t handle;
+
+	if (unlikely(!page_ext))
+		return;
 
 	save_stack_trace(&trace);
 	if (trace.nr_entries != 0 &&
@@ -175,7 +183,15 @@ void __set_page_owner_migrate_reason(struct page *page, int reason)
 {
 	struct page_ext *page_ext = lookup_page_ext(page);
 	if (unlikely(!page_ext))
+<<<<<<< HEAD
 		return;
+=======
+		/*
+		 * The caller just returns 0 if no valid gfp
+		 * So return 0 here too.
+		 */
+		return 0;
+>>>>>>> e34e744... mm: check the return value of lookup_page_ext for all call sites
 
 	page_ext->last_migrate_reason = reason;
 }
