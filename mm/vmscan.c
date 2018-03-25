591f9f523c6e712be13ce7b6068f7405714649ee
Eoghan Cunningham <eoghan61@gmail.com>
2018-03-25 03:26:49 +0100

    Fix vmscan.c build

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -357,7 +357,7 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
 	 * scanning at high prio and therefore should try to reclaim as much as
 	 * possible.
 	 */
-	while (total_scan > min_cache_size ||
+	while (total_scan >= batch_size ||
 	       total_scan >= freeable) {
 		unsigned long ret;
 		unsigned long nr_to_scan = min(batch_size, total_scan);
